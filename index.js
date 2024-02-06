05239d782db02d9f24f7a56adc3298e8aef43916
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-02-06 15:02:47 +0200

    update to notes

--- index.js
+++ index.js
@@ -23,4 +23,36 @@ test.write("Hello World . ");
 test.write("Thank You.");
 test.end();
 
+// Joels Solution
 
+// let fs = require('fs')
+// let path = require('path')
+
+// const filename = path.join(__dirname, './data.txt')
+// function writeToAFile(content){
+//     try{
+//         fs.writeFile(filename, content, err => {
+//             if(!err) console.log('Saved!')
+
+//         })
+//     }catch (e){
+//         console.log('Issue when writing to a file');
+//     }
+// }
+
+// function readFromAFile(){
+//     fs.readFile(filename, 'utf8', (err, data)=>{
+//         if(!err) console.log(data)
+//     }
+//     )
+// }
+
+// function appendToAFile(content){
+//     try{
+//         fs.appendFile(filename, content, err =>{
+//             if(!err) console.log('Your data has been modified')
+//         })
+//     }catch(e){
+//         console.log('Issue when appending data to a file')
+//     }
+// }
