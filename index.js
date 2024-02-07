7f1c64f9aaba515aaea2592d4537f75beb58bcae
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-02-07 16:21:15 +0200

    added more

--- index.js
+++ index.js
@@ -69,3 +69,88 @@ http.createServer((req,res)=>{
 }).listen(port, ()=>{
     console.log(`Server is running on: http://localhost:${port}`)
 })
+
+
+
+//xtra content
+
+
+// fs is a core module also known as a build in module
+// require is to import
+let fs = require('fs')
+let path = require('path')
+
+// finding the path of the file 
+const filename = path.join(__dirname, '')
+
+// function to create a file using keywords as writefile
+// other keywords to create a file are fs.appendFile() fs.open()
+function writeToAFile(content){
+        try{
+            fs.writeFile(filename, content, err => {
+                if(!err) console.log('Saved!')
+    
+            })
+        }catch (e){
+            console.log('Issue when writing to a file');
+        }
+    }
+
+
+    
+// function to read a file using fs module
+//
+function readFromAFile(){
+    fs.readFile(filename, 'utf8', (err, data)=>{
+        if(!err) console.log(data)
+    }
+    )
+}
+
+
+// a function for append to a file 
+// to append means to add or update to a file
+function appendToAFile(content){
+    try{
+        fs.appendFile(filename, content, err =>{
+            if(!err) console.log('Your data has been modified')
+        })
+    }catch(e){
+        console.log('Issue when appending data to a file')
+    }
+}
+
+// another way to create a file is to make use of createwritestream
+// the difference between createwritestream and appendfile
+
+// let test = fs.createWriteStream("Test.txt");
+// test.write("Hello World . ");
+// test.write("Thank You.");
+// test.end();
+
+// delete file
+
+// fs.unlink('Test.txt', function () {
+//     console.log('File Deleted Successfully.');
+// });
+
+
+// fs.appendFile('Test.txt', ' Random text', function (err){
+//     if (err) throw err
+// })
+
+//with writeFile you are overwriting everything, creating the file and adding content to it.
+
+// fs.writeFile('Test.txt', ' Another One', err => {
+//     if(!err) console.log('Saved!')
+// })
+
+// fs.appendFile('Test.txt', ' appending content/ adding', err => {
+//     if(!err) console.log('Saved!')
+// })
+
+// fs.readFile('Test.txt', 'utf8', (err, data)=>{
+//     if(!err) console.log(data)
+// })
+
+
